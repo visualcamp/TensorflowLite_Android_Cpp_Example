c23f622e427cda604c9f1d16f58f1292779e5caa
Tony <cosgenio@gmail.com>
2021-11-28 00:04:19 +0900

    add log

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,10 +1,15 @@
 #include <jni.h>
 #include <string>
 
+#include "log.h"
+
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_example_tensorflowlite_1android_1cpp_1example_MainActivity_stringFromJNI(
-        JNIEnv* env,
-        jobject /* this */) {
-    std::string hello = "Hello from C++";
-    return env->NewStringUTF(hello.c_str());
+    JNIEnv* env,
+    jobject /* this */) {
+  std::string hello = "Hello from C++";
+
+  seeso::Log.d(hello);
+
+  return env->NewStringUTF(hello.c_str());
 }
